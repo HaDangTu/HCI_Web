197896cfa1250ebbda8b8054c1d0b493795bf657
hoangvu98 <hoangvu131098@gmail.com>
2020-01-10 22:01:38 +0700

    update

---
+++ HCI_web/menu_controller.js
@@ -0,0 +1,19 @@
+function onTransferInsideClick(){
+    window.open("../ChuyenTrongHeThong/ChuyenTrongHeThong.html", "_self")
+}
+
+function onPayElectricBillClick(){
+    window.open("../ThanhToanHoaDonTienDien/ThanhToanHoaDonTienDien.html", "_self")
+}
+
+function onTransferOutsideClick(){
+    window.open("../ChuyenNgoaiHeThong/ChuyenNgoaiHeThong.html", "_self");
+}
+
+function onPayPhoneBillClick(){
+    window.open("../NapTienDienThoai/NapTienDienThoai.html", "_self");
+}
+
+function onPayElectricWalletClick(){
+    window.open("../NapTienViDienTu/NapTienViDienTu.html", "_self");
+}
